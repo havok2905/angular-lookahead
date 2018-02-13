0de5dc3f1fe40c8bbf33d1994020520f26f6396a
Chris McLean <havok2905@gmail.com>
2018-02-13 01:51:23 -0500

    scaffold lookahead component

---
+++ src/app/lookahead/lookahead.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lookahead',
+  templateUrl: './lookahead.component.html',
+  styleUrls: ['./lookahead.component.css']
+})
+export class LookaheadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/lookahead/lookahead.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LookaheadComponent } from './lookahead.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LookaheadComponent]
+})
+export class LookaheadModule { }
